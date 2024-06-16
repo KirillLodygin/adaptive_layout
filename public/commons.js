584d6e7d7cb7216e58ac4d58140102ba3c8558cd
Лодыгин Кирилл Валерьевич <lodygin@vtb.ru>
2024-06-16 16:58:12 +0300

    edits

---
+++ public/commons.js
@@ -0,0 +1,7 @@
+export const LOCATION = {
+    center: [37.623082, 55.75254], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+};
+
+export const LONG_TEXT =
+    'Popup on the custom marker (scrollable)\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
